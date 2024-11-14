c938fc450025a1c942a7c12a7a380944213180a1
Utkarsh Bareja <utkarshpant029@gmail.com>
2024-11-14 13:00:48 +0530

    14-11-2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
 import { HomeComponent } from './home/home.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,13 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"ghatfoundation-31872","appId":"1:692774486182:web:bd62163de82c7fbf0cf467","storageBucket":"ghatfoundation-31872.appspot.com","apiKey":"AIzaSyAeCQFhlCCJ5vLslPoIFbCWmOsohYxrGoI","authDomain":"ghatfoundation-31872.firebaseapp.com","messagingSenderId":"692774486182","measurementId":"G-1NRPS013EQ"})),
+    provideAnalytics(() => getAnalytics())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
